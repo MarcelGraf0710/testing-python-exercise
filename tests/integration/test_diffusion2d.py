502df7e91df64259e3c6636ff7cac02b99146986
Marcel Graf <marcel_graf.1@outlook.de>
2026-01-15 17:43:46 +0100

    Completed second integration test

--- tests/integration/test_diffusion2d.py
+++ tests/integration/test_diffusion2d.py
@@ -4,6 +4,7 @@ Tests for functionality checks in class SolveDiffusion2D
 
 from diffusion2d import SolveDiffusion2D
 import pytest
+import numpy as np
 
 
 @pytest.fixture
@@ -37,8 +38,8 @@ def test_set_initial_condition(solver):
     """
     Checks function SolveDiffusion2D.get_initial_function
     """
-    expected_nx = 50
-    expected_ny = 100
+    expected_nx = 1000
+    expected_ny = 2000
 
     input_w = 100.
     input_h = 200.
@@ -51,4 +52,17 @@ def test_set_initial_condition(solver):
     
     solver.initialize_domain(input_w, input_h, input_dx, input_dy)
     solver.initialize_physical_parameters(expected_D, expected_T_cold, expected_T_hot)
+    calculated_u = solver.set_initial_condition()
+
+    expected_u = solver.T_cold * np.ones((expected_nx, expected_ny))
+
+    r, cx, cy = 2, 5, 5
+    r2 = r ** 2
+    for i in range(expected_nx):
+        for j in range(expected_ny):
+            p2 = (i * input_dx - cx) ** 2 + (j * input_dy - cy) ** 2
+            if p2 < r2:
+                expected_u[i, j] = solver.T_hot
+
+    assert np.array_equal(expected_u, calculated_u)
     
