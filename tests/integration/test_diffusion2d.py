68b49c750ffd44bcfa110b842b14996cd0da9c29
Marcel <marcel_graf.1@gmx.de>
2026-01-15 17:06:30 +0100

    Added assertion statements, unit tests, and integration tests

--- tests/integration/test_diffusion2d.py
+++ tests/integration/test_diffusion2d.py
@@ -3,17 +3,52 @@ Tests for functionality checks in class SolveDiffusion2D
 """
 
 from diffusion2d import SolveDiffusion2D
+import pytest
 
 
-def test_initialize_physical_parameters():
+@pytest.fixture
+def solver():
+    solver = SolveDiffusion2D()
+    return solver
+
+
+def test_initialize_physical_parameters(solver):
     """
     Checks function SolveDiffusion2D.initialize_domain
     """
-    solver = SolveDiffusion2D()
+    input_w = 100.
+    input_h = 200.
+    input_dx = 0.1
+    input_dy = 0.1
+
+    expected_T_cold = 100.
+    expected_T_hot = 1000.
+    expected_D = 5.
+
+    expected_dt = 5 * 10e-5
+
+    solver.initialize_domain(input_w, input_h, input_dx, input_dy)
+    solver.initialize_physical_parameters(expected_D, expected_T_cold, expected_T_hot)
+
+    assert solver.dt == pytest.approx(expected_dt, rel=1e-12, abs=0.0)
 
 
-def test_set_initial_condition():
+def test_set_initial_condition(solver):
     """
     Checks function SolveDiffusion2D.get_initial_function
     """
-    solver = SolveDiffusion2D()
+    expected_nx = 50
+    expected_ny = 100
+
+    input_w = 100.
+    input_h = 200.
+    input_dx = 0.1
+    input_dy = 0.1
+
+    expected_T_cold = 100.
+    expected_T_hot = 1000.
+    expected_D = 5.
+    
+    solver.initialize_domain(input_w, input_h, input_dx, input_dy)
+    solver.initialize_physical_parameters(expected_D, expected_T_cold, expected_T_hot)
+    
--- tests/unit/test_diffusion2d_functions.py
+++ tests/unit/test_diffusion2d_functions.py
@@ -3,24 +3,70 @@ Tests for functions in class SolveDiffusion2D
 """
 
 from diffusion2d import SolveDiffusion2D
+import pytest
+import numpy as np
 
 
-def test_initialize_domain():
+@pytest.fixture
+def solver():
+    solver = SolveDiffusion2D()
+    return solver
+
+def test_initialize_domain(solver: SolveDiffusion2D):
     """
     Check function SolveDiffusion2D.initialize_domain
     """
-    solver = SolveDiffusion2D()
+    expected_nx = 50
+    expected_ny = 100
 
+    input_w = 100.
+    input_h = 200.
+    input_dx = 2.
+    input_dy = 2.
+    
+    solver.initialize_domain(input_w, input_h, input_dx, input_dy)
+    assert solver.nx == expected_nx and solver.ny == expected_ny
+    
 
-def test_initialize_physical_parameters():
+def test_initialize_physical_parameters(solver: SolveDiffusion2D):
     """
     Checks function SolveDiffusion2D.initialize_domain
     """
-    solver = SolveDiffusion2D()
+    expected_T_cold = 100.
+    expected_T_hot = 1000.
+    expected_D = 5.
+
+    expected_dt = 5 * 10e-5
+
+    solver.dx = 0.1
+    solver.dy = 0.1
+
+    solver.initialize_physical_parameters(expected_D, expected_T_cold, expected_T_hot)
+
+    assert solver.T_cold == expected_T_cold and solver.T_hot == expected_T_hot and \
+        solver.D == expected_D and solver.dt == pytest.approx(expected_dt, rel=1e-12, abs=0.0)
 
 
-def test_set_initial_condition():
+def test_set_initial_condition(solver: SolveDiffusion2D):
     """
     Checks function SolveDiffusion2D.get_initial_function
     """
-    solver = SolveDiffusion2D()
+    solver.nx = 200
+    solver.ny = 200
+    solver.dx = 0.1
+    solver.dy = 0.1
+    solver.T_cold = 50
+    solver.T_hot = 200
+
+    expected_u = solver.T_cold * np.ones((solver.nx, solver.ny))
+
+    r, cx, cy = 2, 5, 5
+    r2 = r ** 2
+    for i in range(solver.nx):
+        for j in range(solver.ny):
+            p2 = (i * solver.dx - cx) ** 2 + (j * solver.dy - cy) ** 2
+            if p2 < r2:
+                expected_u[i, j] = solver.T_hot
+
+    calculated_u = solver.set_initial_condition()
+    assert np.array_equal(expected_u, calculated_u)
